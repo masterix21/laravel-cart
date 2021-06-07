f49f366f039ab31fa692eac73f2adc0a621c345b
Luca Longo <l.longo@ambita.it>
2021-06-07 12:55:21 +0200

    💅 Add CartManager::total() to gets the cart total

--- src/CartManager.php
+++ src/CartManager.php
@@ -2,6 +2,7 @@
 
 namespace Masterix21\LaravelCart;
 
+use Cknow\Money\Money;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
@@ -167,5 +168,11 @@ class CartManager
     public function contains(Model $model, ?array $meta = null): bool
     {
         return $this->search($model, $meta)->count() > 0;
+    }
+
+    public function total(): Money
+    {
+        return $this->items()
+            ->reduce(fn (?Money $total = null, CartItem $item) => ($total ?? money(0))->add($item->price));
     }
 }
